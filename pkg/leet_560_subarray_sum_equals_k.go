81ed5062b098900d9c94bdd1fe55f863aae53192
Jerry Evarts <jerryevarts@gmail.com>
2023-05-06 19:49:38 +0000

    560 scaffolding

---
+++ pkg/leet_560_subarray_sum_equals_k.go
@@ -0,0 +1,12 @@
+package pkg
+
+// Given an array of integers `nums` and an integer
+// `k` return the total number of subarrays whose sum equals to k.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+// constraints:
+// 1 <= nums.length <= 2 * 104
+// -1000 <= nums[i] <= 1000
+// -107 <= k <= 107
+func subarraySum(nums []int, k int) int {
+	return 0
+}
