d1ced18e0720d48e9b5b5895727cf9ff5bf43849
Jerry Evarts <jerryevarts@gmail.com>
2023-08-02 20:31:20 +0000

    implement DP solution for lc 300

--- pkg/leet_300_longest_increasing_subsequence.go
+++ pkg/leet_300_longest_increasing_subsequence.go
@@ -1,5 +1,25 @@
 package pkg
 
+// lengthOfLIS returns length of longest, increasing subsequence of nums.
 func lengthOfLIS(nums []int) int {
-	return 0
+
+	longestSoFar := []int{}
+	for range nums {
+		longestSoFar = append(longestSoFar, 0)
+	}
+	maxSoFar := 0
+
+	for i, v := range nums {
+		bestYet := 0
+		for k, w := range nums[:i] {
+			if w < v && longestSoFar[k] > bestYet {
+				bestYet = longestSoFar[k]
+			}
+		}
+		longestSoFar[i] = 1 + bestYet
+		if longestSoFar[i] > maxSoFar {
+			maxSoFar = longestSoFar[i]
+		}
+	}
+	return maxSoFar
 }
