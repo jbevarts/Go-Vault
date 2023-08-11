463541c1d193521ca326be946d6d2370c7d091d8
Jerry Evarts <jerryevarts@gmail.com>
2023-08-11 01:17:39 +0000

    trees

--- pkg/datastructures/tree.go
+++ pkg/datastructures/tree.go
@@ -1,1 +1,22 @@
 package datastructures
+
+/*
+	Properties of a Tree:
+	- connected
+	- acyclic
+	- undirected graph
+	- unique path between every vertex
+	- 1 root
+	- N vertices, N-1 edges
+
+	Types of trees:
+	- Binary search tree
+	- Binary tree
+	- AVL tree
+	- B-tree
+	- Red–black tree
+	- Splay tree
+	- Self-balancing binary search tree
+	- M-ary tree
+
+*/
